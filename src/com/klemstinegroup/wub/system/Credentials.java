9a54b49580af6509371ea708badf3529b579daa7
paulklemstine <paul.klemstine@gmail.com>
2020-04-12 13:22:19 -0500

    dumb credential leak

--- src/com/klemstinegroup/wub/system/Credentials.java
+++ src/com/klemstinegroup/wub/system/Credentials.java
@@ -1,6 +1,6 @@
 package com.klemstinegroup.wub.system;
 
 public class Credentials {
-    public static String clientId="3be9c684768d45c891f57c7af41485a9";
-    public static String clientSecret="eb4eb7aae576455fb1487535e30ca471";
+    public static String clientId="dummy";
+    public static String clientSecret="dummy";
 }
